deb2a366526e849235374e8b52956576103da93c
Efe Obieroma <95230993+thesefingers@users.noreply.github.com>
2024-03-06 22:02:14 -0800

    Update Signup.js edit1

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -18,7 +18,7 @@ const Signup = () => {
                     <p>Earn up to 12% annual rewards on 30+ digital assets. Simply hold your assets in the app to automatically earn rewards at the end of each month with no lockups and no limits.</p>
                     <div className='input-container'>
                         <input type='email' placeholder='Enter your email' />
-                        <button className='btn'>Learn More</button>
+                        <a href='https://murqs89r03g.typeform.com/to/HjsA0ont' className='btn'>Learn More</a>
                     </div>
                 </div>
 
